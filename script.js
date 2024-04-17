9b5063fd78149696aa4486c95b967fcfa9c0c708
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-04-17 21:54:48 +0600

    Responsiveness done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let dropdownTag = document.getElementsByClassName("sec-4-dropdown");
 let chevronBtn = document.getElementsByClassName("chevron-btn");
 let showDiv = document.getElementsByClassName("sec-4-show-div");
+let hamBurger = document.getElementsByClassName("nav-bar-hamburger");
+let extraNavbar = document.getElementsByClassName("extra-navbar");
+let hamBurgerClicked = false;
 let isClicked = true;
 
 for (let i = 0; i < dropdownTag.length; i++) {
@@ -17,3 +20,16 @@ for (let i = 0; i < dropdownTag.length; i++) {
     isClicked = !isClicked;
   });
 }
+hamBurger[0].addEventListener("click", (e) => {
+  if (hamBurgerClicked === false) {
+    hamBurger[0].classList.remove("fa-sharp", "fa-bars");
+    hamBurger[0].classList.add("fa-x");
+    extraNavbar[0].style.display = "block";
+  } else {
+    hamBurger[0].classList.remove("fa-x");
+    hamBurger[0].classList.add("fa-sharp", "fa-bars");
+    extraNavbar[0].style.display = "none";
+  }
+  hamBurgerClicked = !hamBurgerClicked;
+});
+// console.dir(extraNavbar);
